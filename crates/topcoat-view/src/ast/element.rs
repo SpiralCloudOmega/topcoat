b156ca27f3d092b729356d405d7f91df43469bea
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2026-04-07 20:24:19 +0200

    Switch to HTML style elements

--- crates/topcoat-view/src/ast/element.rs
+++ crates/topcoat-view/src/ast/element.rs
@@ -1,47 +1,106 @@
 use syn::{
-    Ident,
+    Ident, Token,
     parse::{Parse, ParseStream},
 };
 
 use crate::{
-    ast::{Attributes, ParseOption, node_block::NodeBlock},
+    ast::{Attributes, Node, ParseOption},
     output::ViewWriter,
 };
 
 pub struct Element {
-    pub name: Ident,
-    pub attributes: Attributes,
-    pub body: NodeBlock,
+    pub opening_tag: OpeningTag,
+    pub children: Vec<Node>,
+    pub closing_tag: ClosingTag,
 }
 
 impl Element {
     pub(crate) fn write(&self, writer: &mut ViewWriter) {
         writer.push_str("<");
-        let name = self.name.to_string();
+        let name = self.opening_tag.name.to_string();
         writer.push_str(&name);
-        self.attributes.write(writer);
+        self.opening_tag.attributes.write(writer);
         writer.push_str(">");
 
-        self.body.write(writer);
+        for child in &self.children {
+            child.write(writer);
+        }
 
         writer.push_str("</");
-        writer.push_str(&name);
+        writer.push_str(&self.closing_tag.name.to_string());
         writer.push_str(">");
     }
 }
 
 impl Parse for Element {
     fn parse(input: ParseStream) -> syn::Result<Self> {
+        let opening_tag: OpeningTag = input.parse()?;
+        let mut children = Vec::new();
+        while !input.is_empty() && !ClosingTag::peek(input) {
+            children.push(input.parse()?);
+        }
+        let closing_tag: ClosingTag = input.parse()?;
+        if closing_tag.name != opening_tag.name {
+            return Err(syn::Error::new(
+                closing_tag.name.span(),
+                format!(
+                    "closing tag `{}` does not match opening tag `{}`",
+                    closing_tag.name, opening_tag.name
+                ),
+            ));
+        }
         Ok(Self {
-            name: input.parse()?,
-            attributes: input.parse()?,
-            body: input.parse()?,
+            opening_tag,
+            children,
+            closing_tag,
         })
     }
 }
 
 impl ParseOption for Element {
     fn peek(input: ParseStream) -> bool {
-        input.peek(Ident)
+        input.peek(Token![<])
+    }
+}
+
+pub struct OpeningTag {
+    pub lt: Token![<],
+    pub name: Ident,
+    pub attributes: Attributes,
+    pub gt: Token![>],
+}
+
+impl Parse for OpeningTag {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            lt: input.parse()?,
+            name: input.parse()?,
+            attributes: input.parse()?,
+            gt: input.parse()?,
+        })
+    }
+}
+
+pub struct ClosingTag {
+    pub lt: Token![<],
+    pub slash: Token![/],
+    pub name: Ident,
+    pub gt: Token![>],
+}
+
+impl Parse for ClosingTag {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            lt: input.parse()?,
+            slash: input.parse()?,
+            name: input.parse()?,
+            gt: input.parse()?,
+        })
+    }
+}
+
+impl ParseOption for ClosingTag {
+    fn peek(input: ParseStream) -> bool {
+        input.peek(Token![<]) && input.peek2(Token![/])
     }
 }
--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -2,14 +2,22 @@ fn main() {
     let name = "carl";
 
     let content = topcoat::view! {
-        div {
-            match name {
-                "carl" => b { "hi carl" },
-                "joey" => b { "hi joey" },
-                "julien" => b { "im julien" },
-                _ => "test",
-            }
-        }
+        <html>
+            <head>
+                <title>"hello world"</title>
+            </head>
+            <body>
+                for name in ["carl", "julien"] {
+                    if name.len() < 5 {
+                        <div>
+                            "hello " (name)
+                        </div>
+                    } else {
+                        "im " (name)
+                    }
+                }
+            </body>
+        </html>
     };
 
     println!("{}", content);
